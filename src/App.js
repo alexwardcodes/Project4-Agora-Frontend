8e3e4a7d69ec54de4bdb897b6aa04b40ce50cd73
alex-sasha-ward <amwtranslations@gmail.com>
2024-03-10 15:40:07 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -23,9 +23,9 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
 //icon
 
-export const BASE_URL = process.env.API_URL;
+export const BASE_URL = process.env.REACT_APP_API_URL;
 console.log(BASE_URL)
-console.log(process.env.API_URL)
+console.log(process.env.REACT_APP_API_URL)
 
 export default function App() {
   const [isAuth, setIsAuth] = useState(false);
